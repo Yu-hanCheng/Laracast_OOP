95fe172edf425a6738bb3c50514296b0ca3790e5
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-02-27 08:42:29 +0800

    Objects/static func

---
+++ Section_1/Fib_recursive.php
@@ -0,0 +1,43 @@
+<?php
+class Fib
+{
+
+    function fibV1($n)
+    {
+        if ($n <= 2) {
+            return 1;
+        } else {
+            return $this->fibV1($n - 2) + $this->fibV1($n - 1);
+        }
+    }
+
+    function fibV2($n)
+    {
+        static $result = array();
+        if (!isset($result[$n])) {
+            if ($n <= 2) {
+                $result[$n] = 1;
+            } else {
+                $result[$n] = $this->fibV2($n - 2) + $this->fibV2($n - 1);
+            }
+        }
+        return $result[$n];
+    }
+}
+
+$fib = new Fib(); 
+
+
+$start_time = (float) microtime(true);
+print($fib->fibV1(20));
+$end_time = (float) microtime(true);
+echo "Spent Time: ", ($end_time - $start_time), "(s)\n";
+
+
+$start_time = (float) microtime(true);
+print($fib->fibV2(20));
+$end_time = (float) microtime(true);
+echo "Spent Time: ", ($end_time - $start_time), "(s)\n";
+
+
+?>
---
+++ Section_1/objects.php
@@ -0,0 +1,40 @@
+<?php
+class Team
+{
+    protected $name;
+    protected $members =[];
+
+    public function __construct($name , $members){
+        $this->name = $name;
+        $this->members = $members;
+    }
+
+    public static function start(...$params)
+    {
+        return new self(...$params);
+    }
+
+    public function members(){
+        return $this->members;
+    }
+
+}
+
+class Member{
+    protected $name;
+
+    public function __construct($name){
+        $this->name = $name;
+    }
+
+    public function lastViewed(){
+
+    }
+}
+
+$sarahteam = Team::start('sarah',[
+    new Member('aaa'),
+    new Member('bbb'),
+    ]);
+var_dump($sarahteam);
+?>
