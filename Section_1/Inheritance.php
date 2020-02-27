53f4157958c10dcdf877e310fa0a37c381fd9e57
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-02-27 13:41:13 +0800

    Inheritance

---
+++ Section_1/Inheritance.php
@@ -0,0 +1,36 @@
+<?php
+class Collection
+{
+    protected $items=array();
+    public function __construct(array $items){
+        $this->items = $items;
+    }
+
+    public function sum($key){
+        return array_sum(array_map(function($item) use($key) {
+            return $item->$key;
+        },$this->items));
+    }
+}
+
+class VideoCollection extends Collection{
+    public function length(){
+        return $this->sum('length');
+    }
+}
+class Video 
+{
+    public $title;
+    public $length;
+
+    public function __construct($title, $length){
+        $this->title = $title;
+        $this->length = $length;
+    }
+}
+$collection = new VideoCollection([
+    new Video('1',100),
+    new Video('2',200),
+]);
+echo $collection->length();
+?>
