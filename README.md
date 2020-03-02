a12981018032a581d4e862966884bd0ff7c76033
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-02 11:54:27 +0800

    Object Composition--2 Inheritance

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Object composition IS ABOUT COMMUNICATION.
 
 # NOTE
 1. Option 1:  In Class.
+2. Option 2: Inheritance 
--- Section_1/ObjectComposition.php
+++ Section_1/ObjectComposition.php
@@ -11,6 +11,8 @@ class Subscription
     {
         // api request
         // find stripe customer id
+        $gateway = new StripeGateway();
+        $gateway-> findStripeCustomer();
         // find stripe subscription by customer
     }
     public function invoice()
@@ -22,14 +24,21 @@ class Subscription
 
     }
 
+
+}
+
+class StripeGateway extends Subscription
+{
+    
     protected function findStripeCustomer()
     {
-
+        echo"findStripeCustomer";
     }
 
     protected function findStripeSubscriptionByCustomer()
     {
 
     }
-
 }
+$obj = new Subscription();
+$obj->cancel();
