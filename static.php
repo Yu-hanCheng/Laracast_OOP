61d725aeadae0fc092cbba603eea091b015415a3
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-05 16:52:20 +0800

    Add access static/self example

---
+++ static.php
@@ -0,0 +1,39 @@
+<?php
+
+class Foo
+{
+    public static $my_static = 'foo';
+
+    public function staticValue() {
+        return static::$my_static;
+    }
+
+    public function selfValue() {
+        return self::$my_static;
+    }
+}
+
+class Bar extends Foo
+{
+    public static $my_static = 'bar';
+
+    public function fooValue() {
+        return parent::$my_static;
+    }
+
+    public function barSelfValue() {
+        return self::$my_static;
+    }
+
+    public function barStaticValue() {
+        return static::$my_static;
+    }
+}
+
+$bar = new Bar();
+print $bar->selfValue() . "\n";
+print $bar->staticValue() . "\n";
+print $bar->fooValue() . "\n";
+print $bar->barSelfValue() . "\n";
+print $bar->barStaticValue() . "\n";
+?>
